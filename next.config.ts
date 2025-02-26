681326823f80ce782ebeed04134077d8230a9569
SilentCodeSamurai <silentcodesamurai@gmail.com>
2025-02-26 23:14:28 +0300

    feat: next image deviceSizes

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,24 @@ import MillionLint from "@million/lint";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    output: "standalone",
-}
+	output: "standalone",
+	images: {
+        deviceSizes: [
+            480,
+            640,
+            768,
+            1200,
+        ],
+		remotePatterns: [
+			{
+				protocol: "http",
+				hostname: "localhost",
+				port: "9000",
+				pathname: "/bucket/**",
+				search: "",
+			},
+		],
+	},
+};
 
 export default MillionLint.next({ rsc: true })(nextConfig);
