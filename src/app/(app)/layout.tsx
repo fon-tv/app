3619a3cfa8a299369bca01117772c12e51540168
SilentCodeSamurai <silentcodesamurai@gmail.com>
2025-02-27 08:16:51 +0300

    fix: app layout width

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -9,7 +9,7 @@ export default function AppLayout({
   }>) {
 	return (
 		<div className="flex flex-col lg:pt-[200px]">
-			<div className="z-10 flex flex-col justify-between bg-background mx-0 lg:mx-auto px-[23px] pt-[55px] lg:pt-[49px] pb-[49px] lg:rounded-lg max-w-[1440px] min-h-lvh">
+			<div className="z-10 flex flex-col justify-between bg-background mx-0 lg:mx-auto px-[23px] pt-[55px] lg:pt-[49px] pb-[49px] lg:rounded-lg w-[1440px] max-w-[100%] min-h-lvh">
 				<div className="flex flex-col gap-[10px] lg:gap-[28px]">
 					<Header />
 					<div className="flex flex-col gap-[20px]">
