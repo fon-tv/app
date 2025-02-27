84ee95451f91d7692d7d265a0c32e473acb64131
SilentCodeSamurai <silentcodesamurai@gmail.com>
2025-02-27 08:17:39 +0300

    upd: banner cName

--- src/components/ui/banner.tsx
+++ src/components/ui/banner.tsx
@@ -8,17 +8,17 @@ type BannerProps = ComponentProps<"div"> & {
 	size: "md" | "lg";
 };
 
-export const Banner: React.FC<BannerProps> = ({ imageUrl, imageAlt, href, size, ...props }) => {
-	const className = cn("relative rounded-sm w-full rounded-sm lg:rounded-md overflow-hidden", {
+export const Banner: React.FC<BannerProps> = ({ imageUrl, className, imageAlt, href, size, ...props }) => {
+	const cName = cn("relative rounded-sm w-full rounded-sm lg:rounded-md overflow-hidden", className, {
 		"h-[36.67px] lg:h-[74px]": size === "md",
 		"h-[70px] lg:h-[150px]": size === "lg",
 	});
 	return (
-		<div className={className} {...props}>
+		<div className={cName} {...props}>
 			<img
 				src={imageUrl}
 				alt={imageAlt}
-				className={"object-cover h-full"}
+				className={"object-cover h-full w-full"}
 			/>
 			<a className="top-0 right-0 bottom-0 left-0 absolute" href={href} target="_blank" rel="noreferrer" />
 		</div>
